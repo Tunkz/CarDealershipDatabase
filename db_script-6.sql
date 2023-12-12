68b9881b99cde8febbec44de1c3463227ecdd242
Tunkz <Bubatunkz00@gmail.com>
2023-12-11 20:41:32 -0500

    fixed last bug

--- db_script-6.sql
+++ db_script-6.sql
@@ -1,7 +1,7 @@
 USE cardealershipdatabase;
 SELECT contract_id,`date`,customerName,customerEmail,vehicleVIN
 FROM sales_contracts 
-join inventory on sales_contracts = inventory.VIN
+join inventory on sales_contracts.vehicleVIN = inventory.VIN
 JOIN dealerships on inventory.dealership_id = dealerships.dealership_id
-WHERE dealerships.Name = "Harry"
-AND sales_contracts between "10-10-2000" AND "11-11-2022"; 
+WHERE dealerships.`Name` = "Harry"
+AND sales_contracts.`Date` between "10-10-2000" AND "11-11-2022"; 
